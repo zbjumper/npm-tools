f3e0ac3bb16ca40cdca4cead81c1b573cf8b4f87
张博 <zbjumper@gmail.com>
2026-04-09 13:37:05 +0800

    修正 time 单位

--- src/time.ts
+++ src/time.ts
@@ -30,11 +30,11 @@ export const getTimeAgo = (pastTime: number | string): string => {
 
 /**
  * 格式化时间戳为 "YYYY/MM/DD HH:mm:ss" 格式的字符串
- * @param timestamp 时间戳
+ * @param timestamp 毫秒时间戳
  * @returns 格式化后的时间字符串
  */
 export const formatTime = (timestamp: number | string): string => {
-  const date = new Date(Number(timestamp) * 1000);
+  const date = new Date(Number(timestamp));
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
